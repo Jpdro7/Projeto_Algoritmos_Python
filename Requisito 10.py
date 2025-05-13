b5f8728ca5bb76c31351fecb6fc0b8e0a723858e
João Pedro <almeidanunesjoaopedro2@gmail.com>
2025-05-13 11:21:55 -0300

    Update Requisito 10.py

--- Requisito 10.py
+++ Requisito 10.py
@@ -3,4 +3,9 @@ elif opcao == '0':
                 email_senha_verify = False
                 log_verify = False
                 usuario_log = ''
+                time.sleep(1.50)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print('='* 50)
+                print(f'Bem Vindo'.center(50))
+                print('='* 50)
                 break
